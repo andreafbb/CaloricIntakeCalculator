fde53228b674ce2fa5509db7697c93dd4b312b74
andreafbb <andreaf.bruno@studenti.unina.it>
2026-03-31 19:53:05 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+/*
+Site is located at http://localhost:5173/
+*/
+import Info from "./Info.jsx"
+import Header from "./Header.jsx"
+
+function App() {
+  return(
+    <>
+      <Header/>
+        <Info/>
+    </>
+
+  );
+}
+
+export default App
---
+++ src/Info.jsx
@@ -0,0 +1,124 @@
+
+/*
+This Components contains the logic and graphic elements to implement
+the calculator, with this informations
+
+Mifflin-St Jeor equation
+
+Male
+BMR=10W+6.25H−5A+5
+
+Female
+BMR=10W+6.25H−5A−161
+
+W weight 
+H height (cm)
+A age
+
+And then we multiply di Basal Metabolic Rate to the activity level
+coefficient, with 5 levels:
+
+No activity -> 1.2
+Light activty -> 1.375
+Moderate activity -> 1.55
+Active -> 1.725
+Very Active -> 1.9
+
+TDEE = BMR * Activity Level Coefficient
+
+
+TODO: Implement the Logic and the Result_box
+
+*/
+
+import { useState } from "react";
+
+function Info(){
+
+    const [age, setAge] = useState("");
+    const [height, setHeight] = useState("");
+    const [weight, setWeight] = useState("");
+    const [gender, setGender] = useState("");
+    const [activity, setActivity] = useState("1.2");
+    const [result, setResult] = useState("");
+
+    const calculateBMR = () => {
+        if(!age || !height || !weight || !gender || !activity){
+            setResult("Please insert valid values");
+            return;
+        }
+
+        let BMR = 0
+
+        if(gender == "male"){
+            BMR = (10*weight + 6.25*height + 5 - 5*age).toFixed(0)
+        } else if(gender == "female"){
+            BMR = (10*weight + 6.25*height -161 - 5*age).toFixed(0)
+        }
+
+        let TDEE = calculateTDEE(BMR)
+
+        setResult(`BMR: ${BMR} TDEE: ${TDEE}`);
+
+    }
+
+    const calculateTDEE = (BMR) => {
+        return (BMR*Number(activity)).toFixed(0)
+    }
+
+    return(
+
+        <div>
+            <div className="box">
+
+           
+            <label className="writing_labels" htmlFor="age">Age (18-80)</label>
+            <input type="number" id="age" placeholder="18-80" value={age} onChange={(e) => setAge(e.target.value)} />
+
+          <form>
+            <label className="gender_button" >
+                <input type="radio" name="gender" value="male" checked={gender === "male"}
+                onChange={(e) => setGender(e.target.value)} /> Male
+            </label>
+
+            <label className="gender_button">
+                <input type="radio" name="gender" value="female" checked={gender === "female"}
+                onChange={(e) => setGender(e.target.value)}/> Female
+            </label>
+          </form>
+
+            <label className="writing_labels"  htmlFor="height">Height (cm)</label>
+            <input type="number" id="height" placeholder="170" value={height} onChange={(e) => setHeight(e.target.value)} />
+
+            <label className="writing_labels"  htmlFor="weight">Weight (kg)</label>
+            <input type="number" id="weight" placeholder="60" value={weight} onChange={(e) => setWeight(e.target.value)} />
+
+            <label className="writing_labels"  htmlFor="activity">Activity Level</label>
+                <select className=""
+                             id="activity"
+                            name="activity"
+                            value={activity}
+                            onChange={(e) => setActivity(e.target.value)}
+                            >
+                            <option value="1.2">Sedentary</option>
+                            <option value="1.375">Light Activity</option>
+                            <option value="1.55">Moderate Activity</option>
+                            <option value="1.725">Active</option>
+                            <option value="1.9">Very Active</option>
+                            </select>
+
+            <div className="results_box">
+                <button onClick={calculateBMR}>Calculate</button>
+                <div className="result">
+                {result}
+                </div>
+            </div>
+
+
+            </div>
+        </div>
+
+    );
+};
+
+export default Info
